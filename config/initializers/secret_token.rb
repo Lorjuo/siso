5a061d3fef075e20cc530a2700235b2336d8858a
Julien Seitz <julien.seitz@gmail.com>
2012-12-14 11:02:56 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Siso::Application.config.secret_token = 'ebb97d5d43022c7624cd312973b232fd9ab0952a5e9893d4856334fb7ba723b01000fed234794853cce658ec7efc1812f86a4ad24e2d654968fcf92593a673de'
